792a7a5b621f87f9bae547d5131a63845c83cd6b
ngwanase20 <ayandatemberh@gmail.com>
2025-06-05 13:02:27 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { PrismaClient } from '@prisma/client'
-import authRoutes from './routes/auth.js'
-import startupRoutes from './routes/startups.js'
-import investRoutes from './routes/investments.js'
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./src/routes/auth');
 
-dotenv.config()
-const prisma = new PrismaClient()
-const app = express()
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.use(cors())
-app.use(express.json())
+app.use(express.json());
+app.use('/auth', authRoutes);
 
-app.use('/api/auth', authRoutes)
-app.use('/api/startups', startupRoutes)
-app.use('/api/investments', investRoutes)
+app.get('/', (req, res) => {
+  res.send('API running!');
+});
 
-app.listen(4000, () => {
-  console.log('Backend running at http://localhost:4000')
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
